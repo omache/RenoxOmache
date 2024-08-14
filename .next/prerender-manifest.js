22acd039f73c647aef8e7d62c9d38c38b87c7edc
Qwellstra <109847941+Qwellstra@users.noreply.github.com>
2024-08-14 18:26:21 +0300

    Add .next to .gitignore

--- .next/prerender-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__PRERENDER_MANIFEST="{\"version\":4,\"routes\":{},\"dynamicRoutes\":{},\"preview\":{\"previewModeId\":\"314ce68e7bfde59655468d219d117fe1\",\"previewModeSigningKey\":\"f9e778ea90b186e71cfefe38a1ae61b4b5b39d2e54495e809f22843f4f3654dd\",\"previewModeEncryptionKey\":\"de641907ef304fcc53981155733252bf74fb697ec86de11439bac7ab196f97ed\"},\"notFoundRoutes\":[]}"
